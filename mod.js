09545089ddc9fbe9cf8662d6d5c8980cb60e8d4e
Tobias Buschor <tobias.buschor@shwups.ch>
2021-04-15 16:23:20 +0200

    version

--- mod.js
+++ mod.js
@@ -36,7 +36,7 @@ var urls = {
     'unpkg.com/@ungap/custom-elements@0.1.15/es.js':{
         'customElements':[window],
     },
-    'cdn.jsdelivr.net/gh/nuxodin/lazyfill@0.2.0/polyfills/Element/combo.js':{
+    'cdn.jsdelivr.net/gh/nuxodin/lazyfill@0.2.1/polyfills/Element/combo.js':{
         'matches':[Element],
         'prepend':[Element],
         'append':[Element],
@@ -141,7 +141,7 @@ function createUrls(obj, realObj, rootUrl){
         }
     }
 }
-createUrls(lazyfills, window, 'cdn.jsdelivr.net/gh/nuxodin/lazyfill@0.2.0/polyfills/');
+createUrls(lazyfills, window, 'cdn.jsdelivr.net/gh/nuxodin/lazyfill@0.2.1/polyfills/');
 
 
 var url, props, prop, obj, objects, i;
