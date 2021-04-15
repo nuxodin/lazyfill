4c3e32df94f3e60024dce194766522735f4e68df
Tobias Buschor <tobias.buschor@shwups.ch>
2021-04-15 09:04:03 +0200

    Update at.js

--- polyfills/Array/prototype/at.js
+++ polyfills/Array/prototype/at.js
@@ -1,6 +1,7 @@
 if (!Array.prototype.at) {
     Array.prototype.at = function(n){
-        n = Math.trunc(n) || 0;
+        //n = Math.trunc(n) || 0; not in ie11
+        n = n < 0 ? Math.ceil(n) : Math.floor(n);
         if (n < 0) n += this.length;
         if (n < 0 || n >= this.length) return undefined;
         return this[n];
