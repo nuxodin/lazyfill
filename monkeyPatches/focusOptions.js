957abe0e3629b66451234a3a4e23fd4c98a17253
Tobias Buschor <tobias.buschor@shwups.ch>
2021-07-13 11:29:26 +0200

    Create focusOptions.js

---
+++ monkeyPatches/focusOptions.js
@@ -0,0 +1,32 @@
+
+!function(){
+
+    let supported = false;
+    document.createElement('i').focus({
+        get preventScroll() {
+            supported = true;
+        },
+    });
+
+    if (!supported) {
+        let original = HTMLElement.prototype.focus;
+        Element.prototype.focus = HTMLElement.prototype.focus = function(options){
+            if (options.preventScroll) {
+                const map = new Map();
+                let p = this;
+                while (p = p.parentNode) map.set(p, [p.scrollLeft, p.scrollTop]);
+                original.apply(this, arguments);
+                map.forEach(function(pos, el){
+                    // todo: test: only if changed? does it trigger scroll?
+                    // IE flickers
+                    el.scrollLeft = pos[0]
+                    el.scrollTop  = pos[1]
+                });
+            } else {
+                original.apply(this, arguments);
+            }
+        }
+    }
+
+
+}();
