34c7917b2424d03649654c522d2314eab24930c4
Tobias Buschor <tobias.buschor@shwups.ch>
2022-05-03 01:56:00 +0200

    Create inert.js

---
+++ polyfills/HTMLElement/prototype/inert.js
@@ -0,0 +1,14 @@
+
+if ('inert' in HTMLElement.prototype) {
+
+  Object.defineProperty(HTMLElement.prototype, 'inert', {
+    enumerable: true,
+    get: function() {
+      return this.hasAttribute('inert');
+    },
+    set: function(inert) {
+      inert ? this.setAttribute('inert','') : this.removeAttribute('inert');
+    },
+  });
+
+}
