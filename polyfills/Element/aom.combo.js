028e302222b79dbdf4913854368f2606cd92892f
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-07 16:25:38 +0200

    Update aom.combo.js

--- polyfills/Element/aom.combo.js
+++ polyfills/Element/aom.combo.js
@@ -54,9 +54,8 @@ if (!('computedRole' in Element.prototype)) {
                 return null;
             }
         },
-        set: function(value) {}
+        set: function() {}
     });
-
 
     const roles = {alert:1,alertdialog:1,application:1,article:1,banner:1,button:1,cell:1,checkbox:1,columnheader:1,combobox:1,command:1,complementary:1,composite:1,contentinfo:1,definition:1,dialog:1,directory:1,document:1,feed:1,figure:1,form:1,grid:1,gridcell:1,group:1,heading:1,img:1,input:1,landmark:1,link:1,list:1,listbox:1,listitem:1,log:1,main:1,marquee:1,math:1,menu:1,menubar:1,menuitem:1,menuitemcheckbox:1,menuitemradio:1,navigation:1,note:1,option:1,presentation:1,progressbar:1,radio:1,radiogroup:1,range:1,region:1,roletype:1,row:1,rowgroup:1,rowheader:1,scrollbar:1,search:1,searchbox:1,section:1,sectionhead:1,select:1,separator:1,slider:1,spinbutton:1,status:1,structure:1,tab:1,Table:1,tablist:1,tabpanel:1,Term:1,textbox:1,timer:1,toolbar:1,tooltip:1,tree:1,treegrid:1,treeitem:1,widget:1,window:1,none:1};
 
@@ -101,6 +100,8 @@ if (!('computedRole' in Element.prototype)) {
         'dfn':  [ 'term' ] ,
         'textarea':  [ 'textbox' ] ,
         'input[type=text]': [ 'textbox' ] ,
+        'input:not([type])': [ 'textbox' ] ,
+        'input[type=submit]': [ 'pushbutton' ] ,
     };
 }
 
